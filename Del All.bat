f5bec86143bb8ca41f8a80ff4111a482888e2422
Cyril Ogoh <cyrilogoh@gmail.com>
2020-11-07 19:20:15 +0100

    batch code for File managenment //Del All File and // Creating a New File

---
+++ Del All.bat
@@ -0,0 +1,10 @@
+:: Deletes All files in the Current Directory With Prompts and Warnings 
+::(Hidden, System, and Read-Only Files are Not Affected) 
+:: 
+@ECHO OFF 
+TITLE Deleting All Content
+DEL . 
+DR
+ECHO ============================
+ECHO All Content Deleted
+ECHO ============================
