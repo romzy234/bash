e2fd8dc89128f56e02484925c8c57d29d4536f74
Cyril Ogoh <cyrilogoh@gmail.com>
2020-11-07 19:08:48 +0100

    batch code to listing down all the files in the directory and save in a txt file

---
+++ screengrab.bat
@@ -0,0 +1,5 @@
+@echo off 
+Rem This is for listing down all the files in the directory Program files and save them in a txt file
+dir "PATH ONE" > PATH TWO\list.txt 
+echo "The program has completed"
+PAUSE
